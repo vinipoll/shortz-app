22cc95b18e2f9b8c79ffa2beab8e79640ab18da1
vinipoll <vf005146@gmail.com>
2026-02-19 22:27:54 -0300

    feat: sincronização do banco

--- app.js
+++ app.js
@@ -38,10 +38,18 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//testa a conecxão com o MySQL
+
+
 const sequelize = require('./config/database');
+sequelize.sync({alter:true})
+.then( () => console.log('Sincronia realizada') )
+.catch( err => console.log('Erro de sincronia', err) );
+
+/*
+//testa a conecxão com o MySQL
 sequelize.authenticate()
   .then( ()=> console.log('Conexão com MySQL ok!') )
   .catch( err => console.error('Erro de conexão', err) );
+*/
 
 module.exports = app;
