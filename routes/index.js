f94682f6ff840503942a360769a88fc320c57af7
vinipoll <vf005146@gmail.com>
2026-02-26 22:09:59 -0300

    feat: criado o index da pag

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bem Vindo!' });
+});
+
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Criar conta' });
 });
 
 module.exports = router;
