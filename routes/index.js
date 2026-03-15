6d34020f21b02d9b2e170ce0f4f07b7999569d21
vinipoll <vf005146@gmail.com>
2026-03-15 20:00:12 -0300

    feat: rotas para register e login

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../modules/user/userController');
 
-/* requisição GET para apresentar a home page  */
+// requisição GET para apresentar a home page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bem Vindo!' });
+  res.render('index', { title: 'Vídeos Curtos e Engajadores!' });
 });
 
-
-router.get('/register', function(req, res, next){
+// rota para exibir o formulário de registro
+router.get('/register', (req, res) => {
   res.render('register', {tittle: 'Criar Conta'});
 });
 
+// rota que processa o form de cadastro
+router.post('/register', userController.register);
 
-const userController = require('../modules/user/userController');
-/* requisição GET para apresentar o form de cadastro */
-router.get('/register', userController.register );
+// rota para exibir o form de login
+router.get('/login', (req, res) => {
+   res.render('login', { title: 'Entrar' });
+});
 
 module.exports = router;
