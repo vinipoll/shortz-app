3bc0cd24a6d27ecee1732a53bdc40b523fd3d10e
vinipoll <vf005146@gmail.com>
2026-03-26 21:09:44 -0300

    feat: descentralização de rotas do usuário

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./modules/user/userRoutes');
 
 var app = express();
 var expressLayouts = require("express-ejs-layouts");
@@ -41,7 +41,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', userRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const userController = require('../modules/user/userController');
-const authMiddleware = require('../middlewares/auth');
-const upload = require('../middlewares/multer');
-const auth = require('../middlewares/auth');
 
-// requisição GET para apresentar a home page
+// requisição GET para apresentar a landing page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Vídeos Curtos e Engajadores!' });
+  res.render('landing', { title: 'Vídeos Curtos e Engajadores!' });
 });
-
-// rota para exibir o formulário de registro
-router.get('/register', (req, res) => {
-  res.render('register', {tittle: 'Criar Conta'});
-});
-
-// rota que processa o form de cadastro
-router.post('/register', userController.register);
-
-// rota para exibir o form de login
-router.get('/login', (req, res) => {
-   res.render('login', { title: 'Entrar' });
-});
-
-// Rota para processar o formulário de login
-router.post('/login', userController.login);
-
-// Rota para processar o logout
-router.get('/logout', userController.logout);
-
-// Rota para exibir o feed de vídeos (protegida por autenticação)
-router.get('/feed', authMiddleware, async (req, res) => {
-  const user = await userController.getProfile(req.session.user.id);
-  res.render('home', {user});
-});
-
-// Rota para exibir o perfil do usuário (protegido por autenticação)
-router.get('/profile/edit', authMiddleware, async (req, res) => {
-  const user = await userController.getProfile(req.session.user.id);
-  res.render('edit-profile', {user});
-});
-
-// Rota de atualização (Protegida + Upload de 1 arquivo chamado 'profilePicture')
-router.post('/profile/edit', authMiddleware, upload.single('profilePicture'), userController.updateProfile);
 
 module.exports = router;
