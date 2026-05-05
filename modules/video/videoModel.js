fd2c98b4eb9edabc8ada3165883c5bcee569a9dc
vinipoll <vf005146@gmail.com>
2026-05-05 15:31:17 -0300

    fix: colocado aspas

--- modules/video/videoModel.js
+++ modules/video/videoModel.js
@@ -9,7 +9,7 @@ const Video = sequelize.define("Video",
         videoPath:      { type: DataTypes.STRING(255), allowNull: false },
         thumbnailPath:  { type: DataTypes.STRING(255), allowNull: false },
         views:          { type: DataTypes.INTEGER, defaultValue: 0 },
-        userId:         { type: DataTypes.INTEGER, allowNull: false, references: { model: users, key: "id" } } 
+        userId:         { type: DataTypes.INTEGER, allowNull: false, references: { model: "users", key: "id" } } 
     }, 
     {
         tableName: "videos",
