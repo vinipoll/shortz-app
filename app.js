87698c685d19c2becb4bacd207bb3036fcd974c7
vinipoll <vf005146@gmail.com>
2026-05-05 16:27:57 -0300

    fix: erro ao subir vídeo

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use((req, res, next) => {
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/", indexRouter);
 app.use("/", userRoutes);
